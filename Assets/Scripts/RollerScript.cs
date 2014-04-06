3cd98d310bc93cf9fcb7839a0866c31e112fe6bc
Jill <jg24drive45@gmail.com>
2014-04-05 22:11:23 -0400

    adsf

---
+++ Assets/Scripts/RollerScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.RotateAround( transform.position, Vector3.back, 50 * Time.deltaTime );
+	}
+}
