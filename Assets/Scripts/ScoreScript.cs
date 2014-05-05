fc6bacee2ed4d6e970d94e49e411b8c88ec3decb
mctribble <techgeek1000@gmail.com>
2014-05-05 14:51:36 -0700

    ScoreScript

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreScript : MonoBehaviour {
+
+	private int iPlayerScore;
+	private float fPlayerTime;
+	private bool bTimerActive;
+
+	// Use this for initialization
+	void Start () {
+		iPlayerScore = 0;
+		fPlayerTime = 0.0f;
+		bTimerActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bTimerActive)
+			fPlayerTime += Time.deltaTime; //update timer
+	}
+
+	//start the timper
+	public void StartTimer()
+	{
+		bTimerActive = true;
+	}
+
+	//stop the timer
+
+}
