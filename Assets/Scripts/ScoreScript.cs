c9fbcc5ff75b1394c83a7e88f794d293a38c050f
mctribble <techgeek1000@gmail.com>
2014-05-05 15:28:16 -0700

    ScoreScript

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -1,31 +1,94 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class ScoreScript : MonoBehaviour {
 
-	private int iPlayerScore;
-	private float fPlayerTime;
-	private bool bTimerActive;
+	private int playerScore;	//stores the player's current score
+	private int playerMoveCount;//number of moves the player has made
+	private float playerTime;	//stores the player's current time
+	private bool timerActive;	//whether or not the timer is currently running
 
 	// Use this for initialization
-	void Start () {
-		iPlayerScore = 0;
-		fPlayerTime = 0.0f;
-		bTimerActive = false;
+	void Start () 
+	{
+		playerScore = 0;
+		playerTime = 0.0f;
+		timerActive = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (bTimerActive)
-			fPlayerTime += Time.deltaTime; //update timer
-	}
-
-	//start the timper
-	public void StartTimer()
+	void Update () 
 	{
-		bTimerActive = true;
+		if (timerActive)
+			playerTime += Time.deltaTime; //update timer
 	}
 
-	//stop the timer
+	//display the information
+	void OnGUI()
+	{
+		//divide time into minutes and seconds
+		int iMinutes = (int)fPlayerTime / 60;
+		float fSeconds = playerTime - (iMinutes * 60);
+
+		//create label
+		GUI.Label( new Rect(0,0,512,128), "Time: " + iMinutes + ":" + fSeconds + "\n" +
+		          						  "Score: " + playerScore + "\n" +
+		          						  "Moves: " + playerMoveCount);
+	}
+
+	//-----------accessors-----------//
+
+	public int iPlayerScore
+	{
+		get
+		{
+			return playerScore;
+		}
+
+		set
+		{
+			playerScore = Math.Min(value, 0); //prevent negative score
+		}
+	}
+
+	public int iPlayerMoveCount
+	{
+		get
+		{
+			return playerMoveCount;
+		}
+
+		set
+		{
+			playerMoveCount = value;
+		}
+	}
+
+	public float fPlayerTime
+	{
+		get
+		{
+			return playerTime;
+		}
+
+		set
+		{
+			playerTime = Math.Min(value, 0.0f); //prevent negative time
+		}
+	}
+
+	public bool bIsTimerActive
+	{
+		get
+		{
+			return timerActive;
+		}
+
+		set
+		{
+			timerActive = value;
+		}
+	}
 
 }
