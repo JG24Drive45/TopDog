59d06e647db9a38c5b8969fa171b69b6aad8354d
Jill <jg24drive45@gmail.com>
2014-05-05 18:27:51 -0400

    Created some temp prefabs to use for testing
    ketchup, mustard, relish

--- Assets/Scripts/LevelGeneratorScript.cs
+++ Assets/Scripts/LevelGeneratorScript.cs
@@ -5,6 +5,9 @@ public class LevelGeneratorScript : MonoBehaviour
 {
 
 	public GameObject tempMainTile;
+	public GameObject tempKetchup;
+	public GameObject tempMustard;
+	public GameObject tempRelish;
 
 	// INSTEAD OF GENERATING TILES LIKE BELOW, WE CAN READ DATA IN FROM A TEXT FILE TO GENERATE THE LEVELS
 	void Start () 
@@ -12,11 +15,11 @@ public class LevelGeneratorScript : MonoBehaviour
 		int x, z;
 		z = 0;
 
-		for(int i = 0; i < 5; i++)
+		for(int i = 0; i < 10; i++)
 		{
 			x = 0;
 
-			for(int j = 0; j < 5; j++)
+			for(int j = 0; j < 10; j++)
 			{
 				Instantiate( tempMainTile, new Vector3( x, 0, z), Quaternion.identity );
 				x += 25;
@@ -24,6 +27,11 @@ public class LevelGeneratorScript : MonoBehaviour
 
 			z += 25;
 		}
+
+		// Place the ketchup, mustard, and relish
+		Instantiate( tempKetchup, new Vector3( 125.0f, 0.0f, 0.0f ), Quaternion.identity );
+		Instantiate( tempMustard, new Vector3( 75.0f, 0.0f, 75.0f ), Quaternion.identity );
+		Instantiate( tempRelish, new Vector3( 0.0f, 0.0f, 150.0f ), Quaternion.identity );
 	}
 	
 	// Update is called once per frame
