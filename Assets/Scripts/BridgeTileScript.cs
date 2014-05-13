b5f725a4d831662327f790aa2d30586ad331855d
Jill <jg24drive45@gmail.com>
2014-05-13 02:29:16 -0400

    Level Generator
    Level Generator will read the txt file data of the corresponding scene
    name
    Tiles fixed so that the insides are not broken into quads
    New scenes can now be built

---
+++ Assets/Scripts/BridgeTileScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridgeTileScript : MonoBehaviour {
+
+	private MeshRenderer meshRenderer;
+
+	void Awake()
+	{
+		meshRenderer = GetComponent<MeshRenderer>();
+		meshRenderer.enabled = false;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/LevelGeneratorScript.cs
+++ Assets/Scripts/LevelGeneratorScript.cs
@@ -4,32 +4,37 @@ using System.IO;
 
 public class LevelGeneratorScript : MonoBehaviour 
 {
-
+	// GameObject variables
 	public GameObject mainTile;
 	public GameObject emptyTile;
+	public GameObject teleporterTile;
+	public GameObject bridgeTile;
+	public GameObject fallingTile;
 	public GameObject goalTile;
 	public GameObject ketchup;
 	public GameObject mustard;
 	public GameObject relish;
 	public GameObject player;
+	public GameObject switchObject;
 
-	string sLevel = "Level1";
+	private string sLevel;											// Name of the current level
+
 
 	// INSTEAD OF GENERATING TILES LIKE BELOW, WE CAN READ DATA IN FROM A TEXT FILE TO GENERATE THE LEVELS
 	void Start () 
 	{
-		string[] lines;
+		sLevel = Application.loadedLevelName;						// Get the name of the current level
 
-		TextAsset data;
+		string[] lines;												// Array that stores text file info
+
+		TextAsset data;												// Text file variable
 
 		data = ( TextAsset )Resources.Load( sLevel );
 		lines = data.text.Split( "\n"[0] );
 
-
 		// Read the first 13 lines of text file that make up the tiles
 		for( int i = 2; i < 14; i++ )
 		{
-
 			int lineLength = lines[i].Length;
 			for( int j = 0; j < lineLength; j++ )
 			{
@@ -39,8 +44,16 @@ public class LevelGeneratorScript : MonoBehaviour
 					Instantiate( mainTile, new Vector3( j * 50, 0.0f, -( i - 2 ) * 50 ), Quaternion.Euler( new Vector3( 90, 0, 0 ) ) );
 				else if( lines[i][j] == 'G' )
 					Instantiate( goalTile, new Vector3( j * 50, 0.0f, -( i - 2 ) * 50 ), Quaternion.Euler( new Vector3( 90, 0, 0 ) ) );
+				else if( lines[i][j] == 'T' )
+					Instantiate( teleporterTile, new Vector3( j * 50, 0.0f, -( i - 2 ) * 50 ), Quaternion.Euler( new Vector3( 90, 0, 0 ) ) );
+				else if( lines[i][j] == 'B' )
+				{
+					Instantiate( emptyTile, new Vector3( j * 50, 0.0f, -( i - 2 ) * 50 ), Quaternion.Euler( new Vector3( 90, 0, 0 ) ) );
+					Instantiate( bridgeTile, new Vector3( j * 50, 0.0f, -( i - 2 ) * 50 ), Quaternion.Euler( new Vector3( 90, 0, 0 ) ) );
+				}
+				else if( lines[i][j] == 'F' )
+					Instantiate( fallingTile, new Vector3( j * 50, 0.0f, -( i - 2 ) * 50 ), Quaternion.Euler( new Vector3( 90, 0, 0 ) ) );
 			}
-
 		}
 
 		// Read lines 17 & 18 to get the ketchup location
@@ -58,16 +71,21 @@ public class LevelGeneratorScript : MonoBehaviour
 		Debug.Log( "Relish is at: " + tempPosition );			// For testing
 		Instantiate( relish, tempPosition, Quaternion.Euler( new Vector3( 90.0f, 0.0f, 0.0f ) ) );
 
-		// Read lines 30 - 32 to get the player's (hotdog) location
-		tempPosition = new Vector3( int.Parse( lines[29] ), int.Parse( lines[30] ), -int.Parse( lines[31] ) );
+		// Read lines 26 & 27 to get the switch location
+		tempPosition = new Vector3( int.Parse( lines[25] ), 12.5f, -int.Parse( lines[26] ) );
+		Debug.Log( "Switch is at: " + tempPosition );
+		Instantiate( switchObject, tempPosition, Quaternion.Euler( new Vector3( 270.0f, 0.0f, 0.0f ) ) );
+
+		// Read lines 29 - 31 to get the player's (hotdog) location
+		tempPosition = new Vector3( int.Parse( lines[28] ), int.Parse( lines[29] ), -int.Parse( lines[30] ) );
 		Debug.Log( "Player is at: " + tempPosition );
-		// Read lines 34 - 36 to get the player's (hotdog) rotation
-		Vector3 tempRotation = new Vector3( int.Parse( lines[33] ), int.Parse( lines[34] ), int.Parse( lines[35] ) );
+		// Read lines 33 - 35 to get the player's (hotdog) rotation
+		Vector3 tempRotation = new Vector3( int.Parse( lines[32] ), int.Parse( lines[33] ), int.Parse( lines[34] ) );
 		Debug.Log( "Player rotation is: " + tempRotation );
 		Instantiate( player, tempPosition, Quaternion.Euler( tempRotation ) );
 
-		// Read line 34 to get the player's orientation state
-		int tempState = int.Parse( lines[37] );
+		// Read line 37 to get the player's orientation state
+		int tempState = int.Parse( lines[36] );
 		Debug.Log( "OState is: " + tempState );
 		// Send the message to set the player's orientation state
 		Messenger<int>.Broadcast( "set player orientation state", tempState );
