86505f6ae207dc4e37fdb5bf7da6301077c117c1
Jill <jg24drive45@gmail.com>
2014-06-13 18:15:08 -0400

    readded arrowscript

--- Assets/Scripts/ArrowScript.cs
+++ Assets/Scripts/ArrowScript.cs
@@ -5,10 +5,10 @@ public class ArrowScript : MonoBehaviour
 {
 	public float offset = 0.0f;
 	public float rate = 0.01f;
-
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
@@ -17,7 +17,7 @@ public class ArrowScript : MonoBehaviour
 		offset += rate;
 		if( offset > 1.0f )
 			offset = 0.0f;
-
+		
 		renderer.material.mainTextureOffset = new Vector2( offset, 0.0f );
 	}
 }
