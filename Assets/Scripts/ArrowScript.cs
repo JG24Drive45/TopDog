8addad4b300fe3671be0422e739d4604484a05a0
Jill <jg24drive45@gmail.com>
2014-06-13 18:06:50 -0400

    moving arrow indicators

---
+++ Assets/Scripts/ArrowScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowScript : MonoBehaviour 
+{
+	public float offset = 0.0f;
+	public float rate = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		offset += rate;
+		if( offset > 1.0f )
+			offset = 0.0f;
+
+		renderer.material.mainTextureOffset = new Vector2( offset, 0.0f );
+	}
+}
